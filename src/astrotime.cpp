88e4849f2952ca099bb3b2e1969eec1eb037a6e1
Lars Næsbye Christensen <lars@naesbye.dk>
2015-05-18 20:11:36 +0200

    makefile and menubar

--- src/astrotime.cpp
+++ src/astrotime.cpp
@@ -1,9 +1,16 @@
-// g++ test.cpp `pkg-config --cflags --libs gtk+-2.0`
+// The ESA/ESO/NASA FITS Liberator - http://code.google.com/p/fitsliberator
+//
+// Copyright (c) 2004-2010, ESA/ESO/NASA.
+// All rights reserved.
+// 
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+// 
 
 #include <stdlib.h>
 #include <gtk/gtk.h>
 
-static void helloWorld (GtkWidget *wid, GtkWidget *win)
+static void AstroTime (GtkWidget *wid, GtkWidget *win)
 {
   GtkWidget *dialog = NULL;
 
@@ -16,8 +23,13 @@ static void helloWorld (GtkWidget *wid, GtkWidget *win)
 int main (int argc, char *argv[])
 {
   GtkWidget *button = NULL;
-  GtkWidget *win = NULL;
   GtkWidget *vbox = NULL;
+  GtkWidget *win = NULL;
+
+  GtkWidget *menubar = NULL;
+  GtkWidget *filemenu = NULL;
+  GtkWidget *file = NULL;
+  GtkWidget *quit = NULL;
 
   /* Initialize GTK+ */
   g_log_set_handler ("Gtk", G_LOG_LEVEL_WARNING, (GLogFunc) gtk_false, NULL);
@@ -26,19 +38,33 @@ int main (int argc, char *argv[])
 
   /* Create the main window */
   win = gtk_window_new (GTK_WINDOW_TOPLEVEL);
-  gtk_window_set_default_size(GTK_WINDOW (win), 400, 300);
-  gtk_container_set_border_width (GTK_CONTAINER (win), 8);
-  gtk_window_set_title (GTK_WINDOW (win), "test");
+  gtk_window_set_default_size(GTK_WINDOW (win), 500, 200);
+  gtk_container_set_border_width (GTK_CONTAINER (win), 5);
+  gtk_window_set_title (GTK_WINDOW (win), "AstroTime");
   gtk_window_set_position (GTK_WINDOW (win), GTK_WIN_POS_CENTER);
   gtk_widget_realize (win);
-  g_signal_connect (win, "destroy", gtk_main_quit, NULL);
+ 
+  /* Create a menu bar */
+
+  menubar = gtk_menu_bar_new();
+  filemenu = gtk_menu_new();
+
+  file = gtk_menu_item_new_with_label("File");
+  quit = gtk_menu_item_new_with_label("Quit");
+
+  gtk_menu_item_set_submenu(GTK_MENU_ITEM(file), filemenu);
+  gtk_menu_shell_append(GTK_MENU_SHELL(filemenu), quit);
+  gtk_menu_shell_append(GTK_MENU_SHELL(menubar), file);
+  gtk_box_pack_start(GTK_BOX(vbox), menubar, FALSE, FALSE, 3);
+
+ g_signal_connect (win, "destroy", gtk_main_quit, NULL);
 
   /* Create a vertical box with buttons */
-  vbox = gtk_vbox_new (TRUE, 6);
+  vbox = gtk_vbox_new (TRUE, 5);
   gtk_container_add (GTK_CONTAINER (win), vbox);
 
   button = gtk_button_new_from_stock (GTK_STOCK_DIALOG_INFO);
-  g_signal_connect (G_OBJECT (button), "clicked", G_CALLBACK (helloWorld), (gpointer) win);
+  g_signal_connect (G_OBJECT (button), "clicked", G_CALLBACK (AstroTime), (gpointer) win);
   gtk_box_pack_start (GTK_BOX (vbox), button, TRUE, TRUE, 0);
 
   button = gtk_button_new_from_stock (GTK_STOCK_CLOSE);
