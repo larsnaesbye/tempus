d0f33625efb669924a82f87fd33c653ac46ad771
lanac <lars@naesbye.dk>
2015-05-21 11:25:18 +0000

    pretty headers, app info

--- main.cpp
+++ main.cpp
@@ -1,11 +1,34 @@
+/*
+    Copyright 2015 - Lars Næsbye Christensen
+
+    This is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 3 of the License, or
+    (at your option) any later version.
+
+    The software is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+**/
+
 #include "mainwindow.h"
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
+    QApplication app(argc, argv);
+
+    app.setApplicationName("Tempus");
+    app.setApplicationVersion("0.1");
+    app.setOrganizationDomain("http://github.com/larsnaesbye/tempus");
+    app.setOrganizationName("none");
+
     MainWindow w;
     w.show();
 
-    return a.exec();
+    return app.exec();
 }
